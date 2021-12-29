6d7c5af917c1e9c0534da00b064b16aefc3cfce4
Nicos Angelopoulos <nicos@gmx.co.uk>
2021-12-29 14:07:06 +0000

    DELINK:  ord_only_add_elem.pl

--- src/lib/ord_only_add_elem.pl
+++ src/lib/ord_only_add_elem.pl
@@ -1,1 +1,10 @@
-../../../../../lib/unclean/list/ord_only_add_elem.pl
+ord_only_add_elem( [], Elem, [Elem] ) :- !.
+ord_only_add_elem( [H|T], Elem, NwList ) :-
+	compare( Ord, H, Elem ),
+	( Ord == > ->
+		NwList = [Elem,H|T]
+		;
+		Ord == <,
+		NwList = [H|NwT]
+	),
+	ord_only_add_elem( T, Elem, NwT ).
