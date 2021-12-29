c2f79875f9af356e439108d3157e2b5ae81acd59
Nicos Angelopoulos <nicos@gmx.co.uk>
2021-12-29 10:34:33 +0000

    DELINK:  option_in_values_or_else_default.pl, werr.pl, is_letter.pl, if.pl

--- src/lib/if.pl
+++ src/lib/if.pl
@@ -1,1 +1,27 @@
-../../../../../lib/unclean/meta/if.pl
+%% if( +If, +Then ).
+%% if( +If, +Then, +Else ).
+%
+% Call If, and then THen if it succeeds or Else otherwise.
+% If is called deterministically. If Else is missing, call 'true'.
+% This predicate is syntactic sugar for (->;)
+%==
+% ?- L = [b,a], if( select(a,L,Rem), X = Rem, X = L).
+% Rem = X, X = [b].
+% ?- L = [b,c], if( select(a,L,Rem), X = Rem, X = L).
+% L = X, X = [b, c].
+% ?- L = [b,c], if( select(a,L,Rem), X = Rem ).
+% L = [b, c].
+%==
+%
+% @author nicos angelopoulos
+% @version  0.1 2014/9/10
+%
+if( If, Then ) :-
+	if( If, Then, true ).
+
+if( If, Then, _Else ) :-
+	call( If ),
+	!,
+	once( call(Then) ).
+if( _If, _Then, Else ) :-
+	call( Else ).
--- src/lib/is_letter.pl
+++ src/lib/is_letter.pl
@@ -1,1 +1,4 @@
-../../../../../lib/unclean/codes/is_letter.pl
+is_letter( Letter ) :-
+	( (Letter >= 0'A, Letter =< 0'Z) ;
+	  (Letter >= 0'a, Letter =< 0'z) ),
+	!.
